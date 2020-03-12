5de107bc9ed018b0919f282843e6099371b64f97
Iryna Dunets <idunets@yahoo.com>
2020-03-12 02:06:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i++) {  
+       if (str.includes(bracketsConfig[i].join(''))) {  str = str.replace(bracketsConfig[i].join(''), ''); i=0;}
+  }
+  return str==='';
 }
