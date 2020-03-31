ee5281a77fea5451ffe3a2f2dc47a3cffaab81d7
Iryna Dunets <idunets@yahoo.com>
2020-03-31 21:09:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function check(str, bracketsConfig) { 
  for (let i = 0; i < bracketsConfig.length;) {  
-       if (str.includes(bracketsConfig[i].join(''))) {  str = str.replace(bracketsConfig[i].join(''), ''); i=0;
-       }else{i++}}
+       if (str.includes(bracketsConfig[i].join(''))) {  str = str.replace(bracketsConfig[i].join(''), ''); i = 0;
+       } else { i++; }}
 return str==='';
 }
