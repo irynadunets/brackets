703df9e004db550f23420107ddd7ec7094e64693
Iryna Dunets <idunets@yahoo.com>
2020-04-02 23:41:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-module.exports = function check(str, bracketsConfig) { 
+module.exports = function check(str, bracketsConfig) {
   const n = bracketsConfig.length;
-  for (let i = 0; i < n;) {
-     const brackets = bracketsConfig[i].join('');
-     const contain = str.includes(brackets);
-     if (contain) {  str = str.replace(brackets, ''); i = 0;
-     } else { i++; }
-  }
-return str==='';
+  let i = 0;
+  while ( i < n) {
+     const bracketsTemplate = bracketsConfig[i].join('');
+     const finded = str.includes(bracketsTemplate);
+     if (finded) {
+       str = str.replace(bracketsTemplate, '');
+       i = 0;
+     } else {
+       i++;
+     }
+ }
+return str === '';
 }
+
